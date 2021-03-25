095db41265ffae92fe370991592f03c018a31286
curchadw <curtisdeveloper@gmail.com>
2021-03-24 21:59:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-2drbws
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-2drbws)
---
+++ index.js
@@ -0,0 +1,76 @@
+// // Import stylesheets
+// import './style.css';
+
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+
+
+
+//let operator = *
+//answer = x*x
+//log answer
+//else if operator is equal to /
+//answer = x/x
+//log answer
+
+// let operator = '*';
+// let x =  2;
+// let answer = 2;
+
+
+// function operaton(operator,x,answer){
+// //multiplication
+// if(operator === '*' || operator === 'multiplication'){
+//   answer *= x
+//  console.log(answer)
+// }
+// //division
+// else if(operator === '/' || operator === 'division'){
+//   answer /= x
+//   console.log(answer) 
+
+// }
+// //subtraction
+// else if(operator === '-' || operator === 'subtraction'){
+//   answer -= x
+// console.log(answer)
+
+// }
+// //addition
+// else if(operator === '+' || operator === 'addition'){
+//   answer += x
+//   console.log(answer)
+
+// }
+
+// else{
+//   console.log('Not a valid operator')
+// }
+
+//}
+
+
+// let student1 = 'Curtis'
+// let student2 = 'Josh'
+
+// student1 !== student2 ? console.log('True') : console.log('False');
+
+//Arrays
+//Blocks of memory
+
+let array = [[1,2,3],[4,5,6]]
+for(let i = 0; i < array; i++){
+  for(let j = 0; j < i.length; j++){
+    console.log(array[i][j])
+  }
+  console.log()
+}
+
+
+
+
+
+
